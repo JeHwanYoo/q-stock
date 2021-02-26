3b95b78bb619223a1d098a97b9d074f30e384091
JeHwanYoo <yoojehwan1875@gmail.com>
2021-02-27 01:22:58 +0900

    something

--- src/popup/components/App.vue
+++ src/popup/components/App.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>Hello, Vue!</div>
+  <div>{{ contents }}</div>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({
-  name: "PopupApp"
-})
+import { Component, Vue } from "vue-property-decorator";
+import Axios from "axios";
+
+@Component
+export default class extends Vue {
+  contents = "Not Loaded";
+  code = "005930";
+
+  mounted() {
+    // Axios.get(`http://asp1.krx.co.kr/servlet/krx.asp.XMLSise?code=${this.code}`)
+    //   .then((res) => (this.contents = res.data))
+    //   .catch((err) => (this.contents = err));
+  }
+}
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
---
+++ src/popup/components/query.ts
@@ -0,0 +1,7 @@
+function queryCurrentMarketPrice() {
+    
+}
+
+export {
+    queryCurrentMarketPrice
+}
